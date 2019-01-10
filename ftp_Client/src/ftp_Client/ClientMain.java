98397f11e93292f721da92870007c7b75bcc3a59
unknown <maakni.kahina13@gmail.com>
2019-01-10 10:58:17 +0100

    fonctions upload et download finalisées

--- ftp_Client/src/ftp_Client/ClientMain.java
+++ ftp_Client/src/ftp_Client/ClientMain.java
@@ -1,18 +1,155 @@
 package ftp_Client;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStream;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
-import org.apache.commons.net.ftp.*;
+import java.awt.GridLayout;
+import java.awt.event.*;
+import java.awt.Dimension;
 
-public class ClientMain {
+public class ClientMain extends JFrame {
+
+	 private javax.swing.JPanel jContentPane = null;
+	 private JTree              jTreeLocal        = null;
+	 private JTree              jTreeRemote        = null;	 
+	   JLabel l1, l2, l3;
+       JTextField tf1;
+       JButton btn1;
+       JPasswordField p1;
+	 
+	ClientMain(){
+		super ("ClientFtp");
+	      WindowListener l = new WindowAdapter() {
+	          public void windowClosing(WindowEvent e){
+	             System.exit(0);
+	          }
+	       };
+	       
+	       
+	       //
+	    
+	      
+	        JFrame frame = new JFrame("Login Form");
+	        l1 = new JLabel("Login Form");
+	        l1.setForeground(Color.blue);
+	        l1.setFont(new Font("Serif", Font.BOLD, 20));
+	       
+	        l2 = new JLabel("Username");
+	        l3 = new JLabel("Password");
+	        tf1 = new JTextField();
+	        p1 = new JPasswordField();
+	        btn1 = new JButton("Login");
+	       
+	        l1.setBounds(100, 30, 400, 30);
+	        l2.setBounds(80, 70, 200, 30);
+	        l3.setBounds(80, 110, 200, 30);
+	        tf1.setBounds(300, 70, 200, 30);
+	        p1.setBounds(300, 110, 200, 30);
+	        btn1.setBounds(150, 160, 100, 30);
+	       
+	        frame.add(l1);
+	        frame.add(l2);
+	        frame.add(tf1);
+	        frame.add(l3);
+	        frame.add(p1);
+	        frame.add(btn1);
+	       
+	        frame.setSize(400, 400);
+	        frame.setLayout(null);
+	        frame.setVisible(true);
+
+	       
+	       //
+
+	       addWindowListener(l);
+	       setSize(200,100);
+	       setVisible(true);
+	       
+	       //Instancier la fênetre principale
+			final JPanel composantsAPlacer = new JPanel();
+			GridLayout grid = new GridLayout(2,3);
+			composantsAPlacer.setLayout(grid);
+
+			
+
+			// Instancier le champs login password
+			
+			JTextField  testField1 = new JTextField ("mon texte");
+			composantsAPlacer.add(testField1);
+			
+			
+			
+			JPasswordField  passwordField1 = new JPasswordField ("");
+			passwordField1.setPreferredSize(new Dimension(50,20 ));
+			composantsAPlacer.add(passwordField1);
+			composantsAPlacer.add(new JButton("Button "));
+
+			
+			
+			
+			//composantsAPlacer.add(new JButton("Button 2 "));
+			composantsAPlacer.add(getLocal());
+
+			JPanel panel2 = new JPanel();
+			GridLayout grid2 = new GridLayout(3,3);
+
+			panel2.setLayout(grid2);
+
+			composantsAPlacer.add(getRemote());
+
+			panel2.add(new JButton("Button 4 "));
+			panel2.add(new JButton("Button 5 "));
+			panel2.add(new JButton("Button 6"));
+			panel2.add(new JButton("Button 7"));
+			panel2.add(new JButton("Button 8 "));
+
+			this.setContentPane(composantsAPlacer);
+
+	  
+	}
+
+	private JTree getLocal() {
+		if (jTreeLocal == null) {
+			jTreeLocal = new JTree();
+		}
+		return jTreeLocal;
+	}
+	private JTree getRemote() {
+		if (jTreeRemote == null) {
+			jTreeRemote = new JTree();
+		}
+		return jTreeRemote;
+	}
+
+
+	private javax.swing.JPanel getJContentPane() {
+		if (jContentPane == null) {
+		    jContentPane = new javax.swing.JPanel();
+		    jContentPane.setLayout(new java.awt.BorderLayout());
+		    jContentPane.add(getLocal(), java.awt.BorderLayout.CENTER);
+		  }
+		 return jContentPane;
+	}
+	
 
 	
 	public static void main(String[] args) {
+		
+		JFrame frame = new ClientMain();
+
+				
+		
 	//	Client client = new Client("127.0.0.1", 21);
-		String local = "C:\\ftp";
+		String local = "C:\\ftp\\testFTP";
 
 		ConnexionFtp connexion_server= null;
        
@@ -21,9 +158,26 @@ public class ClientMain {
             //directories =  ftpClient.listDirectories();
             //directories =  ftpClient.changeWorkingDirectory(pathname);
         	
-            String filePath = "/commons-net-3.6";
-
+        	
+			
+            String filePath = "/test";
+            
+            /*Ebauche de la fonction UploadFileOrDirectory
+            String remote="testserv.txt";
+            String local2= "C:\\Users\\Kahina\\Desktop\\test2.txt";
+            InputStream input = new FileInputStream(local2);
+            connexion_server.getFtpClient().storeFile( remote,  input);
+            */
+            String remote="/";
+            String local2= "C:\\Users\\Kahina\\Desktop\\test";
+            
+            //test fonction upload
+            //connexion_server.uploadFileOrDirectory(remote, local2);
+            
+             //tester la fonction download
             connexion_server.getFileOrDirectory(filePath,local);
+            
+            
             //String time = connexion_server.getFtpClient().getModificationTime(filePath);
             //System.out.println("Server Reply: " + time);
             
@@ -50,13 +204,8 @@ public class ClientMain {
         }
         if((connexion_server==null)) {
         	System.out.println("Error Null pointer");
-        }
-        
-        /*try {
-			connexion_server.displayDir("/");
-		} catch (IOException e) {
-			System.err.println(e.getMessage());
-		}*/
+       }      
+	
 	}
 
 }
--- ftp_Client/src/ftp_Client/ConnexionFtp.java
+++ ftp_Client/src/ftp_Client/ConnexionFtp.java
@@ -2,9 +2,11 @@
 package ftp_Client;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.SocketException;
 
@@ -30,54 +32,102 @@ public class ConnexionFtp {
 		this.ftpClient = ftpClient;
 	}
 	
-	public void getFileOrDirectory(String remote, String local) throws IOException {
-        //FTPFile[] content = this.getFtpClient().listFiles(remote);
-       /*for( FTPFile file:content ){
-		
-        	if(file.isFile()) {
-        		local+=file.getName();
-        		OutputStream output;
-        		output = new FileOutputStream(local);
-        		this.getFtpClient().retrieveFile(remote, output);
-                System.out.println(  " File :"+remote+" created in :"+local );
-        	}else if(file.isDirectory()) {
-        		local+= '\\'+file.getName();
-        		
-                new File(local).mkdirs();
-                System.out.println(  " Directory :"+file.getName()+" created in :"+local );
-
-               getFileOrDirectory(remote+"/"+ file.getName(), local );
-        	}
-        }*/
-        
-        
+	public void uploadFileOrDirectory(String remote, String local) {
 		try{
-            FTPFile[] files = this.ftpClient.listFiles(remote);
-            for ( FTPFile file : files){
+			File folder = new File(local);
+			if(folder.isDirectory()) {
+                String newRemote  = remote + '\\'+ folder.getName();
 
-                if(file.isFile()){
-                    System.out.println(file.getName() + " est un fichier");
-
-                    String newLocal = local + '\\' + file.getName();
-                    try(OutputStream output = new FileOutputStream(newLocal))
-                    {
-                        this.getFtpClient().retrieveFile(remote + '\\' + file.getName(), output);//output endroit ou il met le fichier
-                    }//récuperartion du file dans le dossier remote
-                    catch(FileNotFoundException ex){
-                        ex.printStackTrace();//affichage des erreurs à l'ouverture des fichiers
-                    }
-                    catch(IOException ex){
-                        ex.printStackTrace();
-                    }
+                this.getFtpClient().makeDirectory(newRemote);
+                this.getFtpClient().changeWorkingDirectory(newRemote);
+                File[] listOfFiles = folder.listFiles();
+                
+                for ( File file : listOfFiles){
+		            	if(file.isFile()){
+		                    //System.out.println(file.getName() + " est un fichier");
+		                    try(InputStream input = new FileInputStream(file.getAbsolutePath())){
+		                        this.getFtpClient().storeFile(file.getName(), input);	//input endroit ou il recupere le fichier
+		                    }																			//recuperartion du file dans le dossier remote
+		                    catch(FileNotFoundException ex){
+		                        ex.printStackTrace();													//affichage des erreurs a l'ouverture des fichiers
+		                    }
+		                    catch(IOException ex){
+		                        ex.printStackTrace();
+		                    }
+		            	}
+		            	else if(file.isDirectory()){
+		                    //System.out.println(file.getName() + " est un dossier.");
+		                    String newPath  = this.getFtpClient().printWorkingDirectory() + '\\'+ file.getName();
+		                    this.getFtpClient().makeDirectory(newPath);
+		                    //System.out.println("dossier cree");
+		                    //System.out.println(local + '/'+ file.getName());// 
+		                    uploadFileOrDirectory(newRemote ,local + '/'+ file.getName());// on va lister tout ce qui se trouve dans le sous-repertoire
+		                    this.getFtpClient().changeToParentDirectory();
+		                }
+	            	} 
                 }
-                else if(file.isDirectory()){
-                    System.out.println(file.getName() + " est un dossier.");
-                    String newLocal  = local + '\\'+ file.getName();
-                    new File(newLocal).mkdirs();
-                    System.out.println("dossier créer");
-                    System.out.println(remote + '/'+ file.getName());// 
-                    getFileOrDirectory(remote + '/'+ file.getName(),newLocal);// on va lister tout ce qui se trouve dans le sous-repertoire
-
+			else {
+			    try(InputStream input = new FileInputStream(folder.getAbsolutePath())){
+                    this.getFtpClient().storeFile(folder.getName(), input);	//input endroit ou il recupere le fichier
+                }																			//recuperartion du file dans le dossier remote
+                catch(FileNotFoundException ex){
+                    ex.printStackTrace();													//affichage des erreurs a l'ouverture des fichiers
+                }
+                catch(IOException ex){
+                    ex.printStackTrace();
+                }
+			}
+			
+			}catch(Exception ex){
+                ex.printStackTrace();
+			}
+		
+	}
+	
+	public void getFileOrDirectory(String remote, String local) throws IOException {       
+		try {
+            FTPFile[] files = this.ftpClient.listFiles(remote);
+            if(files.length == 0) {
+            	 try(OutputStream output = new FileOutputStream(local))
+                 {
+                     this.getFtpClient().retrieveFile(remote, output);//output endroit ou il met le fichier
+                 }//recuperartion du file dans le dossier remote
+                 catch(FileNotFoundException ex){
+                     ex.printStackTrace();//affichage des erreurs a l'ouverture des fichiers
+                 }
+                 catch(IOException ex){
+                     ex.printStackTrace();
+                 }
+            }
+            else {
+            	//crÃ©e dossier puis parcourir dossier 
+            	String[] splitResult = remote.split("/");
+            	File folder = new File(local +'\\'+ splitResult[splitResult.length-1]);
+            	folder.mkdirs();
+            	for ( FTPFile file : files){
+                    if(file.isFile()){
+                        //System.out.println(file.getName() + " est un fichier");
+                        String newLocal = folder.getAbsolutePath() + '\\' +  file.getName();
+                        try(OutputStream output = new FileOutputStream(newLocal))
+                        {
+                            this.getFtpClient().retrieveFile(remote + '\\' + file.getName(), output);//output endroit ou il met le fichier
+                        }//recuperartion du file dans le dossier remote
+                        catch(FileNotFoundException ex){
+                            ex.printStackTrace();//affichage des erreurs a l'ouverture des fichiers
+                        }
+                        catch(IOException ex){
+                            ex.printStackTrace();
+                        }
+                    }
+                    else if(file.isDirectory()){
+                        
+                        String newLocal  =  folder.getAbsolutePath();
+                        
+                        System.out.println("dossier cree");
+                        System.out.println("Remote : " + remote + '/'+ file.getName());//
+                        System.out.println("local : " + newLocal);//
+                        getFileOrDirectory(remote + '/'+ file.getName(),newLocal);// on va lister tout ce qui se trouve dans le sous-repertoire
+                    }
                 }
             }
         }
